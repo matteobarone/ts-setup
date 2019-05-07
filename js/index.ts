ce571cd4a035f3f1ff8f6313869ba2d425ef2d70
matteobarone <barmat90@gmail.com>
2019-05-07 23:30:58 +0200

    feat(map): test mappa

--- js/index.ts
+++ js/index.ts
@@ -1,18 +1,39 @@
-import Module from './module';
 import '../css/index.css';
+import {Observable} from "rxjs/index";
+import {distinct, switchMap} from "rxjs/internal/operators";
+import { interval } from 'rxjs';
+const L = require('leaflet');
 
-function fetchData(): Promise<any> {
-  return fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json());
+const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
+
+const quakesObservable = new Observable((observer) => {
+  (window as any).eqfeed_callback = (response: any) => {
+    observer.next(response);
+    observer.complete();
+  };
+  loadJSONP(QUAKE_URL);
+});
+
+const quakes = interval(2000).pipe(
+  switchMap(() => quakesObservable),
+  switchMap((res: any) => res.features),
+  distinct((quake: any) => quake.properties.code),
+);
+
+function loadJSONP(url) {
+  const script = document.createElement('script');
+  script.src = url;
+  const head = document.getElementsByTagName('head')[0];
+  head.appendChild(script);
 }
 
-function test(): void {
-  const instance = new Module();
-  console.log(instance.test);
+(() => {
+  const map = L.map('map').setView([33.858631, -118.279602], 7);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-  fetchData().then(res => {
-    console.log(res);
-  })
-}
-
-test();
+  quakes.subscribe((quake: any) => {
+    const coords = quake.geometry.coordinates;
+    const size = quake.properties.mag * 10000;
+    L.circle([coords[1], coords[0]], size).addTo(map);
+  });
+})();
