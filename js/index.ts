8880d0c4d903d4f9979876ccc348878780a4cde7
Matteo Barone <barmat90@gmail.com>
2019-05-08 18:36:44 +0200

    feat(map): bubble

--- js/index.ts
+++ js/index.ts
@@ -1,10 +1,15 @@
 import '../css/index.css';
-import {Observable} from "rxjs/index";
-import {distinct, switchMap} from "rxjs/internal/operators";
-import { interval } from 'rxjs';
+import {map, switchMap} from "rxjs/operators";
+import {interval, Observable} from 'rxjs';
+import {zip} from 'rxjs/internal/observable/zip';
+import {of} from 'rxjs/internal/observable/of';
+import {filter} from 'rxjs/internal/operators/filter';
+
 const L = require('leaflet');
 
 const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
+
+const timer = interval(300);
 
 const quakesObservable = new Observable((observer) => {
   (window as any).eqfeed_callback = (response: any) => {
@@ -14,12 +19,6 @@ const quakesObservable = new Observable((observer) => {
   loadJSONP(QUAKE_URL);
 });
 
-const quakes = interval(2000).pipe(
-  switchMap(() => quakesObservable),
-  switchMap((res: any) => res.features),
-  distinct((quake: any) => quake.properties.code),
-);
-
 function loadJSONP(url) {
   const script = document.createElement('script');
   script.src = url;
@@ -28,12 +27,29 @@ function loadJSONP(url) {
 }
 
 (() => {
-  const map = L.map('map').setView([33.858631, -118.279602], 7);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  const quakeMap = L.map('map').setView([33.858631, -118.279602], 7);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(quakeMap);
 
-  quakes.subscribe((quake: any) => {
-    const coords = quake.geometry.coordinates;
-    const size = quake.properties.mag * 10000;
-    L.circle([coords[1], coords[0]], size).addTo(map);
+  quakesObservable.subscribe(res => {
+    timer.pipe(
+      switchMap((t) => {
+        return zip(of(t), of(res));
+      }),
+      switchMap(([time, quakes]) => {
+        const resQuakes = (quakes as any).features;
+        return zip(of(time), of(resQuakes));
+      }),
+      filter(([time, quakes]) => {
+        return quakes[time];
+      }),
+      map(([time, quakes]) => {
+        return quakes[time];
+      })
+    ).subscribe((quake: any) => {
+      console.log(quake);
+      const coords = quake.geometry.coordinates;
+      const size = quake.properties.mag * 10000;
+      L.circle([coords[1], coords[0]], size).addTo(quakeMap);
+    });
   });
 })();
